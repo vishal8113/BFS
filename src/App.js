f7bc61bdc102337d713a2179347ad63ea183e9cf
Vishal Chauhan <92880117+vishal8113@users.noreply.github.com>
2025-05-21 14:33:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -81,11 +81,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          { <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} /> */}
-          <Route path="/" element={<WebDown />} />
+          <Route path="/contact" element={<Contact />} /> 
         </Routes>
       </Router>
     </ThemeProvider>
